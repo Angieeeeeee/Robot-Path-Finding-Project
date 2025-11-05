31cdb3ee9cff3603de6a3d4556dbd763841427e2
Angieeeeeee <80398192+Angieeeeeee@users.noreply.github.com>
2025-11-05 00:20:54 +0000

    initial code commit

---
+++ brushfire.py
@@ -0,0 +1,39 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+hub = PrimeHub()
+
+# ------------------------------------------------------------------------------
+#  Hardware Initialization 
+# ------------------------------------------------------------------------------
+
+leftMotor = Motor(Port.C)
+rightMotor = Motor(Port.B)
+
+# ------------------------------------------------------------------------------
+#  Global Variables 
+# ------------------------------------------------------------------------------
+
+rows = 16
+cols = 10 
+maze = np.zeros((rows, cols))
+
+numObstacles = 13
+obstacles = []
+start = 
+end = 
+
+# ------------------------------------------------------------------------------
+#  Path Finding
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+#  Main
+# ------------------------------------------------------------------------------
+
+leftMotor.run(-200)
+rightMotor.run(200)
